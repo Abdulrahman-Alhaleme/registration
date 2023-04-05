89868ea5e1ece9f1319b108a6207b9d8beaa5ad2
Abdulrahman-Alhaleme <abdulrahman.alhaleme@gmail.com>
2023-04-04 23:17:38 -0700

    update

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function saveData(event) {
     event.preventDefault();
 
-    // Check if username exists in session storage
+
     const username = document.getElementById("username").value;
-    const userData = sessionStorage.getItem(username);
+    const userData = localStorage.getItem(username);
     if (userData) {
         alert("Username already exists. Please choose a different username.");
         return;
     }
 
-    // Save form data to session storage
+
     const password = document.getElementById("password").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
@@ -19,7 +19,7 @@ function saveData(event) {
         email,
         phone
     };
-    sessionStorage.setItem(username, JSON.stringify(data));
+    localStorage.setItem(username, JSON.stringify(data));
     alert("Registration successful!");
     location.reload();
 }
